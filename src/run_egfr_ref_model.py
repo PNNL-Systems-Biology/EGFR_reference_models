d38cd7f76fe45156c546981cff812cb382614638
mialko <kochen.michael@gmail.com>
2023-06-21 20:30:36 -0700

    Minor fixes.

--- src/run_egfr_ref_model.py
+++ src/run_egfr_ref_model.py
@@ -3,6 +3,6 @@ import tellurium as te
 
 r = te.loada('EGFR_Reference_Model.ant')
 
-# sim = r.simulate(0, 10, 11, ['time', 'EGFR_aRtot'])
+# sim = r.simulate(0, 10, 11, ['time', 'Grb2_SOS1_input'])
 sim = r.simulate(0, 10, 11)
 print(sim)
