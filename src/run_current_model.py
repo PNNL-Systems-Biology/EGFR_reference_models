021262bea8d1f73b2348faa8c5ef6cb0be944108
mialko <kochen.michael@gmail.com>
2023-08-05 22:04:46 -0700

    Minor updates.

--- src/run_current_model.py
+++ src/run_current_model.py
@@ -23,16 +23,20 @@ erk_time = np.array(erk_df['Time'])
 erk = np.array(erk_df['Rp'])
 
 r = te.loada('current_model.ant')
-sim = r.simulate(0, 10, 101, selections=['time', 'ppERK'])
-print(11, sim['ppERK'])
+r.integrator.absolute_tolerance = 1e-12
+r.integrator.relative_tolerance = 1e-12
+sim = r.simulate(0, 10, 1001, selections=['time', 'RAS', 'ppERK', 'RASb_pRAF_d1433u_MEK'])
+print(11, sim['RASb_pRAF_d1433u_MEK'])
 
-t = np.linspace(0, 10, 101)
+t = np.linspace(0, 10, 1001)
 print(t)
 # quit()
-plt.plot(t, cubic(t), c='blue', label='RAS curve')
-plt.plot(t, sim['ppERK'], c='orange', label='ERK fit')
-plt.scatter(time, Rp, c='blue', label='RAS data')
-plt.scatter(erk_time, erk, c='orange', label='ERK data')
+# plt.plot(t, cubic(t), c='blue', label='RAS curve')
+# plt.plot(t, sim['ppERK'], c='purple', label='RAS_out')
+# plt.plot(t, sim['ppERK'], c='orange', label='ERK fit')
+plt.plot(t, sim['RASb_pRAF_d1433u_MEK'], c='green', label='RAFa trace')
+# plt.scatter(time, Rp, c='blue', label='RAS data')
+# plt.scatter(erk_time, erk, c='orange', label='ERK data')
 plt.legend()
 plt.show()
 
